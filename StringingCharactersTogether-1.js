0781c80a5df38c45d1f7bc23cb5e506f9e1e057f
BryJime <bry.jimenex@gmail.com>
2025-03-14 13:50:33 -0700

    Finished Assignment for Stringing Characters Together - 1

---
+++ StringingCharactersTogether-1.js
@@ -0,0 +1,9 @@
+const readline = require('readline-sync');
+
+let prompt = readline.question("Enter your prompt: "); 
+
+let indexFind = readline.question("Enter an index number: ");
+
+console.log(prompt[indexFind]);
+
+
